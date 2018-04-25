a8c41841a6d1bef0348b5010096db1c92e563320
Training_C2a.05.01 <j.john.mariyadoss@accenture.com>
2018-04-25 13:10:19 +0530

    add 1

--- src/test/java/com/tests/NewTest2.java
+++ src/test/java/com/tests/NewTest2.java
@@ -19,14 +19,14 @@ public class NewTest2 {
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_c2a.05.01\\Desktop\\Chrome\\chromedriver.exe");
 		driver=new ChromeDriver();
-		driver.get("http://newtours.demoaut.com/");
+		driver.get("http://google.com/");
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.name("userName")).sendKeys("remi");
+		/* driver.findElement(By.name("userName")).sendKeys("remi");
 		driver.findElement(By.name("password")).sendKeys("remi");
 		driver.findElement(By.name("login")).click();
 		String title=driver.getTitle();
-		Assert.assertEquals(title, "Find a Flight: Mercury Tours:");
+		Assert.assertEquals(title, "Find a Flight: Mercury Tours:");     */
 	
 		
 	}
